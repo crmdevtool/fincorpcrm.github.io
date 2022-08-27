1925f1f93fffe0d7f7865afe9aec21f87b29672f
adimael <adimaelbr@gmail.com>
2022-08-26 23:01:52 -0300

    first commit

---
+++ components/layout.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default function Layout({ children }) {
+    return (
+        <div className='flex flex-col min-h-screen' >
+            <header className='bg-[#0da08f] mb-8 py-4'>
+            <div className='container mx-auto flex justify-center'>
+                <Link href='/'>
+                    <a>🏡Home</a>
+                </Link>
+                <span className='mx-auto'>Welcome to my blog</span>{' '}
+            </div>
+            </header>
+            <main className='container mx-auto flex-1'>{children}</main>
+            <footer className='bg-[#0da08f] mt-8 py-4'>
+                <div className='container mx-auto flex justify-center'>
+                    &copy; 2022 Adimael
+                </div>
+            </footer>
+        </div>
+    );
+}
---
+++ posts/o-que-e-github-e-como-usa-lo.md
@@ -0,0 +1,14 @@
+---
+title: 'O Que é GitHub e Como Usá-lo'
+metaTitle: 'O Que é GitHub e Como Usá-lo'
+metaDesc: 'GitHub é considerado é uma ferramenta essencial para engenheiros de software'
+socialImage: images/github-26-08-2022.png
+date: '2022-08-26'
+tags:
+  - github
+---
+## Para melhor entender o que é o GitHub e como ele funciona, precisamos dar uma olhada mais a fundo.
+
+  O GitHub é considerado é uma ferramenta essencial para engenheiros de software, com uma popularidade sem igual. Atualmente, ele acomoda mais de 25 milhões de usuários. Isso significa que há um número considerável de profissionais que estão procurando o GitHub para melhorar o fluxo de trabalho e a colaboração.
+
+![Isso é uma imagem](https://myoctocat.com/assets/images/base-octocat.svg)
